26a36d0c20228d18b7c06aeb2fdbbbc87cc2c216
bearinawolfpack <bearinawolfpack@gmail.com>
2021-06-29 14:02:13 -0400

    Integrate Mongoose

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
+
+mongoose
+  .connect('mongodb://localhost:27017/farmstand', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to the farmstand database!'))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`ExpressMongoWarmup listening at http://localhost:${port}`);
