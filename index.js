ea5a6ca133506ed34969135ea9974aa0af6b1f79
bearinawolfpack <bearinawolfpack@gmail.com>
2021-06-30 17:22:04 -0400

    Add index route and render index view

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ const port = 3001;
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 const Product = require('./models/products');
+const ejs = require('ejs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
+});
+
+app.get('/', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
+});
+
+
+mongoose
+.connect('mongodb://localhost:27017/farmStore', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to the farmStore database!'))
+.catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`ExpressMongoWarmup listening at http://localhost:${port}`);
 });
 
 // GET products/index -> URL /products -> Show all products
@@ -16,15 +38,3 @@ app.get('/', (req, res) => {
 // GET products/edit -> URL /products/edit/:id -> Edit product form PATCH
 // PATCH redirect to URL /products
 // DELETE redirect to URL /products
-
-mongoose
-  .connect('mongodb://localhost:27017/farmStore', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to the farmStore database!'))
-  .catch((err) => console.log(err));
-
-app.listen(port, () => {
-  console.log(`ExpressMongoWarmup listening at http://localhost:${port}`);
-});
