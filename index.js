e5c7364a0448625522e4c23499ec85e3d355ce63
bearinawolfpack <bearinawolfpack@gmail.com>
2021-07-01 12:12:08 -0400

    Add new route to index
    Created template html for new.ejs

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 
-app.get('/product/:id', async (req, res) => {
+// GET products/new -> URL /products/new -> New product form POST
+app.get('/products/new', (req, res) => {
+  res.render('products/new');
+});
+
+// GET products/show -> URL /products/:id -> Show one product
+app.get('/products/:id', async (req, res) => {
   const id = req.params.id;
   const product = await Product.findById(id);
   res.render('products/show', { product });
 });
 
+// GET products/index -> URL /products -> Show all products
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
   res.render('products/index', { products });
@@ -36,9 +43,6 @@ app.listen(port, () => {
   console.log(`ExpressMongoWarmup listening at http://localhost:${port}`);
 });
 
-// GET products/index -> URL /products -> Show all products
-// GET products/show -> URL /products/:id -> Show one product
-// GET products/new -> URL /products/new -> New product form POST
 // POST redirect to URL /products
 // GET products/edit -> URL /products/edit/:id -> Edit product form PATCH
 // PATCH redirect to URL /products
